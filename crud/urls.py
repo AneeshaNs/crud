e71e6b556d9c05b47bb549391ab5e5d74458e6a6
AneeshaNs <aneeshansv@gmail.com>
2022-04-07 09:19:48 +0530

    CRUD Operation

---
+++ crud/urls.py
@@ -0,0 +1,14 @@
+from django.conf.urls import url
+from django.urls import path
+
+from . import views
+
+urlpatterns= [
+
+   path('', views.index, name='index'),
+    path('create', views.create, name='create'),
+    path('edit/<id>', views.edit, name='edit'),
+    path('edit/update/<id>', views.update, name='update'),
+    path('delete/<id>', views.delete, name='delete'),
+
+]
